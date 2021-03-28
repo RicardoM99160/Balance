bba63274ead81f1b5e9e8f63cab3002a8d1190d2
RicardoM99160 <inuriqui_cool2@outlook.com>
2021-03-28 16:59:26 -0600

    Diseño Homepage sin tareas - problema al correrlo en mi celular

--- Balance/app/src/main/java/com/domos/balance/MainActivity.java
+++ Balance/app/src/main/java/com/domos/balance/MainActivity.java
@@ -2,13 +2,25 @@ package com.domos.balance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void crearTarea(View view){
+        Intent intent = new Intent(this,CreateTask.class);
+        startActivity(intent);
+    }
+
+
 }
