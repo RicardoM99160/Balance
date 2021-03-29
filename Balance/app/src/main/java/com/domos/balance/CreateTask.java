ce163ef17d33126c2a647019e818eb4c8bda694d
RicardoM99160 <inuriqui_cool2@outlook.com>
2021-03-29 13:52:59 -0600

    Creacion de clase Task

--- Balance/app/src/main/java/com/domos/balance/CreateTask.java
+++ Balance/app/src/main/java/com/domos/balance/CreateTask.java
@@ -6,9 +6,17 @@ import android.os.Bundle;
 
 public class CreateTask extends AppCompatActivity {
 
+    Task newTask;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
+
+        newTask = new Task();
+
     }
+
+
 }
--- Balance/app/src/main/java/com/domos/balance/MainActivity.java
+++ Balance/app/src/main/java/com/domos/balance/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
+    HashMap<String,Task> Tasks = new HashMap<String, Task>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    //Creo que este seria ejecutado cuando termine de crear la nueva tarea
+    //En el onResume se recibe la nueva tarea y se almacena en la lista de tareas
+    //Si no funciona así tendría que crear algun evento o algo así
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Obtengo la tarea de la actividad CreateTask
+        //Estoy utilizando serialization porque tengo un problema con los booleanos en Parcelable
+        Task newTask = (Task) getIntent().getSerializableExtra("newTask");
+        Tasks.put(newTask.getId(),newTask);
+
+    }
+
+
+
     public void crearTarea(View view){
         Intent intent = new Intent(this,CreateTask.class);
         startActivity(intent);
     }
-
 
 }
