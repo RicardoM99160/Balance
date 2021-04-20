a67b66f078ed8db9a1d591186cedf7c71a5281df
RicardoM99160 <inuriqui_cool2@outlook.com>
2021-04-20 17:06:01 -0600

    diseñando login

--- Balance/app/src/main/java/com/domos/balance/Wellcome.java
+++ Balance/app/src/main/java/com/domos/balance/Wellcome.java
@@ -2,13 +2,29 @@ package com.domos.balance;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Wellcome extends AppCompatActivity {
+
+    Button btnInicioSesion, btnRegistrarse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wellcome);
+
+        btnInicioSesion = (Button) findViewById(R.id.wBtnLogin);
+        btnRegistrarse = (Button) findViewById(R.id.wBtnSignUp);
+
+        btnInicioSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Wellcome.this, Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
